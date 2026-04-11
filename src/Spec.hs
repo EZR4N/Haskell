fbe9433297ee0542f205a8cc42f198f248d176fc
Ezequiel Orellana <124017711+EZR4N@users.noreply.github.com>
2026-04-10 23:21:07 -0300

    excercise one

--- src/Spec.hs
+++ src/Spec.hs
@@ -7,5 +7,5 @@ correrTests :: IO ()
 correrTests = hspec $ do
   describe "Test de ejemplo" $ do
     it "El pdepreludat se instaló correctamente" $ do
-      doble 1 `shouldBe` 2
+      esMultiploDe3 6 `shouldBe` True
 
