7052cad609f926ece1d1b22e89f7d22cf20f89bb
EZR4N <ezeore2@gmail.com>
2026-04-18 00:09:10 -0300

    3-1

--- src/Library.hs
+++ src/Library.hs
@@ -4,5 +4,5 @@ import PdePreludat
 doble :: Number -> Number
 doble numero = numero + numero
 
-positivo :: Number -> Bool
-positivo = (>=0)
+multiploDe2 :: Number -> Bool
+multiploDe2  = (== 0) . (`mod` 2)
