313ba98acf2c64dad4a88bb69f38dd3ec43ad66b
EZR4N <ezeore2@gmail.com>
2026-04-18 22:50:33 -0300

    4-2

--- src/Library.hs
+++ src/Library.hs
@@ -3,3 +3,12 @@ import PdePreludat
 
 doble :: Number -> Number
 doble numero = numero + numero
+
+triple :: Number -> Number
+triple = (*3)
+
+type GenericNumberFunction = (Number -> Number)
+type Tuple = (GenericNumberFunction, GenericNumberFunction)
+
+aplicar :: Tuple -> Number -> (Number, Number)
+aplicar (f, g) x = (f x, g x)
