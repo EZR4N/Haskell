1e0c23bc659a0bc4e3a457d8a5051c7918b284d3
EZR4N <ezeore2@gmail.com>
2026-04-23 12:46:47 -0300

    5-1: sumar lista

--- src/Library.hs
+++ src/Library.hs
@@ -3,3 +3,10 @@ import PdePreludat
 
 doble :: Number -> Number
 doble numero = numero + numero
+
+sumarLista :: [Number] -> Number
+sumarLista [] = 0
+sumarLista (x : xs) = x + sumarLista xs
+
+sumarLista' :: [Number] -> Number
+sumarLista' = sum
