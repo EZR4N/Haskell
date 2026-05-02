8d46576878a8f94ddb9d0b48587fd6053e64ab2b
EZR4N <ezeore2@gmail.com>
2026-05-02 14:02:01 -0300

    listas-2

--- src/Library.hs
+++ src/Library.hs
@@ -3,3 +3,25 @@ import PdePreludat
 
 doble :: Number -> Number
 doble numero = numero + numero
+
+data FrecuenciasPorMinuto = Frecuencias{
+    minuto :: Number,
+    frecuencia :: Number
+} deriving (Eq, Show)
+
+frecuenciasCardiacas :: [FrecuenciasPorMinuto]
+frecuenciasCardiacas = [
+    Frecuencias 0 80,
+    Frecuencias 10 100,
+    Frecuencias 20 120,
+    Frecuencias 30 128,
+    Frecuencias 40 130,
+    Frecuencias 50 123,
+    Frecuencias 60 125
+  ]
+
+sumadeFrecuencias :: [FrecuenciasPorMinuto] -> Number
+sumadeFrecuencias = sum . map frecuencia 
+
+promedioFrecuencias :: [FrecuenciasPorMinuto] -> Number
+promedioFrecuencias lista = (sumadeFrecuencias lista) / (length lista)
