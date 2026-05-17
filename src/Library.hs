dff4f87f53ec909593565d53b61d384913c4babb
EZR4N <ezeore2@gmail.com>
2026-05-16 23:00:47 -0300

    sacar prom sublistas

--- src/Library.hs
+++ src/Library.hs
@@ -3,3 +3,10 @@ import PdePreludat
 
 doble :: Number -> Number
 doble numero = numero + numero
+
+promedios :: [[Number]] -> [Number]
+promedios = map sacarPromedio
+
+sacarPromedio :: [Number] -> Number
+sacarPromedio [] = 0
+sacarPromedio lista = sum lista / (length lista) 
