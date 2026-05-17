af0f09d2d60eecbf18d7213282bf2afb542cb384
EZR4N <ezeore2@gmail.com>
2026-05-16 22:51:18 -0300

    esMultiploDeAlguno

--- src/Library.hs
+++ src/Library.hs
@@ -3,3 +3,9 @@ import PdePreludat
 
 doble :: Number -> Number
 doble numero = numero + numero
+
+esMultiploDe :: Number -> Number -> Bool
+esMultiploDe n = (== 0) . (`mod` n) 
+
+esMultiploDeAlguno :: Number -> [Number] -> Bool
+esMultiploDeAlguno n lista = any (esMultiploDe n) lista
